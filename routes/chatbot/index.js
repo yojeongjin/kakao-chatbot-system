84ddffe79ff88764e07fae7d37614b699e819680
jeongjin o <jeongjino@jeongjinui-noteubug.local>
2025-05-17 01:28:42 +0900

    README 추가

--- routes/chatbot/index.js
+++ routes/chatbot/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-// agreement
 const agreement = require('./agreement');
+const inquiry = require('./inquiry');
+const cancel = require('./cancel');
+const photo = require('./photo');
+
+router.use('/agreement', agreement);
+router.use('/inquiry', inquiry);
+router.use('/cancel', cancel);
+router.use('/photo', photo);
 
 router.all('*', (req, res) => {
   res.status(404).send({ success: false, msg: `chatbot unknown uri ${req.path}` });
